6224135cd29c71ac559a6e91b29413b4d9eb85c6
Casper Kaandorp <c.s.kaandorp@uu.nl>
2022-09-20 19:42:53 +0200

    connection between websocket server and react established

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './images/logo.svg';
 import { Counter } from './features/counter/Counter';
 import { ServerStatus } from './features/serverstatus/ServerStatus';
+
+import WebSocketCall from "./components/WebSocketCall";
+import { io } from "socket.io-client";
+
 import './styles/App.scss';
 
+
 function App() {
+  const [socketInstance, setSocketInstance] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [buttonStatus, setButtonStatus] = useState(false);
+
+  const handleClick = () => {
+    if (buttonStatus === false) {
+      setButtonStatus(true);
+    } else {
+      setButtonStatus(false);
+    }
+  };
+
+  useEffect(() => {
+    if (buttonStatus === true) {
+      const socket = io("http://127.0.0.1:5001", {
+        transports: ["websocket"],
+        cors: {
+          origin: "http://localhost:3000/",
+        },
+      });
+
+      // setSocketInstance(socket);
+
+      socket.on("connect", (data) => {
+        console.log('connected: ', data);
+      });
+
+      setLoading(false);
+
+      socket.on("disconnect", (data) => {
+        console.log('disconnected: ', data);
+      });
+
+      setSocketInstance(socket);
+
+      // clean up
+      return function cleanup() {
+        socket.disconnect();
+      };
+    }
+  }, [buttonStatus]);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <ServerStatus />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
+      <p>Hello</p>
+
+      {!buttonStatus ? (
+        <button onClick={handleClick}>turn chat on</button>
+      ) : (
+        <>
+          <button onClick={handleClick}>turn chat off</button>
+          <div className="line">
+            {!loading && <WebSocketCall socket={socketInstance} />}
+          </div>
+        </>
+      )}
     </div>
   );
 }
